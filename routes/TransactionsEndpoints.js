b6f5e848dc446c980cb7863befa85cca5f16c0d7
Zeeshan <66442918+zeeshan-thedeveloper@users.noreply.github.com>
2022-02-25 02:36:57 +0500

    update

--- routes/TransactionsEndpoints.js
+++ routes/TransactionsEndpoints.js
@@ -63,13 +63,107 @@ module.exports={
 
     getSentFundsTransactionsListByAccountAddressRouter:getSentFundsTransactionsListByAccountAddressRouter.post("/getSentFundsTransactionsListByAccountAddress",(req,resp)=>{
         const {targetAccountAddress}=req.body;
+        var transactionList =[];
+        //getting block number so that we can traverse till that block.
+        web3.eth.getBlockNumber()
+        .then((currentTotalBlocknumber)=>{
+            //Getting total transactions done from
+            web3.eth.getTransactionCount(targetAccountAddress)
+            .then((totalDoneTransactionsByTargetAccount)=>{
+                //Now we will traverse the block chain till we have found number 
+                //of transactions equal to totalDoneTransactionsByTargetAccount
+                const tempTransDataHolder=[];
+                
+                let traverseBlocks = new Promise( function (traverseBlock_Reject,traverseBlock_Resolve) {
+                    
+                    for(var blockNumber=0;blockNumber<currentTotalBlocknumber;){
+                            // web3.eth.getBlock(blockNumber).then((b_detail)=>{
+                            // })
+                        let acessEachBlock = new Promise(function (acessEachBlock_Reject,acessEachBlock_Resolve) {
+                         setInterval(() => {
+                            acessEachBlock_Resolve("Go ahead")
+                         }, 3000);
+                                    //     // let getTransFromBlock = new Promise(new function (getTransFromBlock_Reject,getTransFromBlock_Resolve) {
+                                    //     //     //This will be resolve when a block is accesses and traversed.. end of each for-each..
+                                    //     // })
+                                    //     //This will be resolve when all blocks are access or when outer loop is ended..
+                        
+                        })
 
-        var transactionList;
-        resp.status(200).send({
-            responsePlayload:transactionList,
-            responseMessage:"List of your, Mr :"+targetAccountAddress+", spent fund's transactions : "+transactionList,
-            responseCode:808
+                        //This will be resolve when all blocks are traversed.
+                        acessEachBlock.then((sucess)=>{
+                            console.log("Increasing index :"+sucess)
+                            blockNumber++;
+                        },(error)=>{
+                            console.log(error)
+                        })
+                    }
+                    traverseBlock_Resolve(tempTransDataHolder)
+                })
+
+                traverseBlocks.then((sucess)=>{
+                    resp.status(200).send({
+                        responsePlayload:sucess,
+                        responseMessage:"List of your, Mr :"+targetAccountAddress+", spent fund's transactions : "+transactionList,
+                        responseCode:808
+                    })
+                },(error)=>{
+                    resp.status(200).send({
+                        responsePlayload:error,
+                        responseMessage:"List of your, Mr :"+targetAccountAddress+", spent fund's transactions : "+transactionList,
+                        responseCode:808
+                    })
+                })
+                
+
+                // for(var blockNumber=0;blockNumber<currentTotalBlocknumber;blockNumber++){
+
+                //     web3.eth.getBlock(blockNumber).then((b_detail)=>{
+                //         if(b_detail.transactions.length>0){
+                //             b_detail.transactions.forEach(function(e) {
+                //                 web3.eth.getTransaction(e)
+                //                 .then((transactionReciept)=>{
+                //                     // console.log(transactionReciept)
+                //                     if(transactionReciept.from.toLowerCase()==targetAccountAddress.toLowerCase())
+                //                     {
+                //                         // console.log(transactionReciept.to)
+                //                         tempTransDataHolder.push({
+                //                         toHash:transactionReciept.to,
+                //                         ammount:web3.utils.fromWei(transactionReciept.value, 'ether')
+                //                         })
+                //                         console.log(tempTransDataHolder)
+                //                     }
+                //                 })
+                //             })
+                //         }
+
+                //     })
+                // }
+
+                // console.log(tempTransDataHolder)
+                // {
+                //     responsePayload:[
+                //         {
+                //             transactionSentFromHashAddress:"",
+                //             transactionSentToHashAddress:"",
+                //             transactionAmount:"",
+                //             transactionSentToFirstName:"",
+                //             transactionSentToLastName:"",
+                //             transactionSentToEmail:"",
+                //             transactionSentToAccountHashAddress:"",
+                //             transactionHash:""
+                //         },
+                //         {....},
+                //         {....}
+                //     ],
+                //     responseMessage:"Transactions list :",
+                //     responseCode:805
+                // }
+                
+            })
         })
+
+        
     }),
 
     getRecievedFundsTransactionsListByAccountAddressRouter:getRecievedFundsTransactionsListByAccountAddressRouter.post("/getRecievedFundsTransactionsListByAccountAddress",(req,resp)=>{
